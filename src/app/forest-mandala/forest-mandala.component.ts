208a9616ca2a5bca8e126d76cbb36e95f23d69e9
melmepham <m.a.mepham@gmail.com>
2019-04-07 20:42:54 +1200

    making triangles

--- src/app/forest-mandala/forest-mandala.component.ts
+++ src/app/forest-mandala/forest-mandala.component.ts
@@ -37,6 +37,10 @@ export class ForestMandalaComponent implements OnInit {
   let green100 = p.color('#ebffeb');
   let green400 = p.color('#00b02c');
 
+  let oceanGreen400 = p.color('#00dfb2');
+  let oceanBlue200 = p.color('#a6e5ff');
+  let oceanBlue400 = p.color('#009fe1');
+
     window.onresize = function() {
       canvasSize = p.windowWidth / 1.5;
       p.resizeCanvas(canvasSize, canvasSize);
@@ -60,16 +64,33 @@ export class ForestMandalaComponent implements OnInit {
       p.center.x = p.width / 2;
       p.center.y = p.height / 2;
 
+      //circle framing the flower of life
       p.push();
       p.translate(p.center.x, p.center.y);
-      p.stroke(green400);
-      p.fill(green400);
+      p.noStroke();
+      p.fill(oceanBlue200);
+      p.ellipse(0, 0, petal * 2, petal * 2)
+      p.pop();
+
+      //triange WIP
+      p.push();
+      p.translate(p.center.x, p.center.y);
+      p.triangle(-66, 66, -66, -33, 0, 66)
+      p.pop()
+
+      //flower of life
+      p.push();
+      p.translate(p.center.x, p.center.y);
+      p.stroke(oceanGreen400);
+      p.fill(oceanGreen400);
+      p.rotate(30);
       for (let i=0; i<6; i++) {
         p.curve(petal, 0, 0, 0, 0, petal, petal, petal);
         p.curve(-petal, 0, 0, 0, 0, petal, -petal, petal);
         p.rotate(60);
-      }
+      };
       p.pop();
+
     };
   };
 }
