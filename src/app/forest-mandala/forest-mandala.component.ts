3d7820eb5d02f1ae43ce4454d928d7535ce210ac
Melissa Mepham <melissa.mepham@trademe.co.nz>
2019-04-11 17:45:26 +1200

    added some circles and changed color of petals

--- src/app/forest-mandala/forest-mandala.component.ts
+++ src/app/forest-mandala/forest-mandala.component.ts
@@ -43,7 +43,8 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
   let Atriangex1, Atriangley1, Atriangley2, Btriangley1;
   let AcircleSize, AcircleX;
   let BcircleX, BcircleSize;
-  let flowerX1, flowerY1, flowerY2;
+  let flowerX1, flowerY1, flowerY2, flowerCircle;
+  let CCircleWH, CCircleXY, DCircleWH;
 
   // setup vars
   let canvasSize;
@@ -63,6 +64,10 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
     flowerX1 = canvasSize / 15.1;
     flowerY1 = canvasSize / 9;
     flowerY2 = canvasSize / 5.5;
+    flowerCircle = canvasSize / 3.8;
+    CCircleXY = canvasSize / 10;
+    CCircleWH = canvasSize / 20;
+    DCircleWH = canvasSize / 15;
   }
 
     window.onresize = function() {
@@ -99,14 +104,15 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
       p.center.x = p.width / 2;
       p.center.y = p.height / 2;
 
+
       // flowerA
       p.push();
         p.translate(p.center.x, p.center.y);
-        p.fill(this._c.hotPink200);
+        p.fill(p.color('#7cdd6d'));
         p.push();
           p.noStroke();
-          p.fill(this._c.hotPink200);
-          p.ellipse(0, 0, canvasSize/3.8, canvasSize/3.8);
+          p.fill(p.color('#7cdd6d'));
+          p.ellipse(0, 0, flowerCircle, flowerCircle);
         p.pop();
         p.push();
           p.noStroke();
@@ -114,11 +120,24 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
             p.beginShape();
             p.vertex(-flowerX1, -flowerY1);
             p.bezierVertex(-flowerX1, -flowerY2, 0 ,-flowerY1, 0, -flowerY2);
-            p.bezierVertex(0 ,-flowerY1, flowerX1, -flowerY2, flowerX1, -flowerY1);
+            p.bezierVertex(0, -flowerY1, flowerX1, -flowerY2, flowerX1, -flowerY1);
             p.endShape();
             p.rotate(60);
           }
         p.pop();
+      p.pop();
+
+      // CCircle
+      p.push();
+        p.translate(p.center.x, p.center.y);
+        for (let i = 0; i < 6; i++) {
+          p.noStroke();
+          p.fill(p.color(this._c.oceanGreen400));
+          p.ellipse(0, -CCircleXY, DCircleWH, DCircleWH);
+          p.fill(p.color('#EEFAF7'));
+          p.ellipse(0, -CCircleXY, CCircleWH, CCircleWH);
+          p.rotate(60);
+        }
       p.pop();
 
       // Bcircles
@@ -127,7 +146,7 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
         p.noStroke();
         p.push()
         p.fill(this._c.oceanBlue400);
-          p.scale(1.2)
+          p.scale(1.2);
           p.rotate(p.radians(p.frameCount / 3) * -50);
           for (let i = 0; i < 6; i++) {
             p.ellipse(-BcircleX, 0, BcircleSize, BcircleSize);
@@ -143,7 +162,7 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
             p.ellipse(-BcircleX, 0, BcircleSize, BcircleSize);
             p.rotate(60);
           }
-          p.pop()
+          p.pop();
       p.pop();
 
       // Atriange
@@ -171,7 +190,7 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
       p.fill(this._c.hotPink200);
       p.scale((p.sin(p.frameCount / 3) * 1.3) + .5);
       for (let i = 0; i < 6; i++) {
-        p.ellipse(-AcircleX, 0, AcircleSize, AcircleSize)
+        p.ellipse(-AcircleX, 0, AcircleSize, AcircleSize);
         p.rotate(60);
       }
       p.pop();
@@ -180,9 +199,9 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
       p.push();
       p.translate(p.center.x, p.center.y);
       p.scale((p.sin(p.frameCount / 3) * 1.3) + .5);
-      p.noStroke()
+      p.noStroke();
       p.fill(255, 255, 255, 200);
-      p.ellipse(0, 0, petal * 2 , petal * 2)
+      p.ellipse(0, 0, petal * 2 , petal * 2);
       p.pop();
 
       // flower of life
@@ -192,7 +211,7 @@ export class ForestMandalaComponent implements OnInit, OnDestroy {
       p.stroke(255, 255, 255, 0);
       p.scale((p.sin(p.frameCount / 4) * .5) + .5);
       p.rotate(p.radians(p.frameCount / 4) * -50);
-      p.fill(p.color(this._c.yellowGreen200))
+      p.fill(p.color(this._c.yellowGreen200));
       for (let i = 0; i < 6; i++) {
         p.curve(petal, 0, 0, 0, 0, petal, petal, petal);
         p.curve(-petal, 0, 0, 0, 0, petal, -petal, petal);
